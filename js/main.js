c02f093b32b832b9ea980f967837724e8c036e93
AlB <albert.burc@gmail.com>
2017-08-17 19:10:19 -0400

    added modal error functionality and removal based upon user actions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var buttonSignIn = document.querySelector('.signin');
 var buttonClose = document.querySelector('.close');
+var buttonSubmit = document.querySelector('.submit');
+var inputField = document.querySelectorAll('form > input')
 
 1.
 buttonSignIn.addEventListener('click', function(eventObject) {
@@ -12,3 +14,16 @@ buttonClose.addEventListener('click', function(eventObject) {
 })
 
 3.
+buttonSubmit.addEventListener('click', function(eventObject) {
+  user.classList.add('error')
+  pass.classList.add('error')
+})
+
+4.
+  inputField.forEach(function(i) {
+    i.addEventListener('mouseover', function () {
+      i.className = '';
+    })
+  })
+
+5.
